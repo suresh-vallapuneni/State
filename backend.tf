e91130ae16f4c86e8ab448d2fac61f19664c905e
vallapuneni suresh <suresh@infy.com>
2026-06-27 16:59:43 +0530

    add

--- backend.tf
+++ backend.tf
@@ -1,1 +1,10 @@
 
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-suresh-2026"
+    key            = "dev/terraform.tfstate"
+    region         = "ap-south-2"
+    dynamodb_table = "terraform-state-lock"
+    encrypt        = true
+  }
+}
