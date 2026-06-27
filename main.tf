09f34bde0a71c4e517800b07cc61afcd8b704713
vallapuneni suresh <suresh@infy.com>
2026-06-27 16:45:54 +0530

    add

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ resource "aws_s3_bucket" "demo_bucket" {
   bucket = var.bucket_name
 
   tags = {
-    Name        = "Terraform Demo Bucket"
+    Name        = "Terraform Demo Bucket UPDATED"
     Environment = "Dev"
+    Owner       = "Suresh"
   }
 }
