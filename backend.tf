2432e847588a856698cfa1d0e49ad15c307802c0
suresh-vallapuneni <sureshvallapuneni9@gmail.com>
2026-06-27 16:29:46 +0530

    Remove Terraform backend configuration

--- backend.tf
+++ backend.tf
@@ -1,9 +1,1 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-suresh-2026"
-    key            = "dev/terraform.tfstate"
-    region         = "ap-south-2"
-    dynamodb_table = "terraform-state-lock"
-    encrypt        = true
-  }
-}
+
